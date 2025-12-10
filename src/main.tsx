5f66656d9b0022b3207f19924e5ee1060ccb91c4
WilliamSalcedo <wsl_1994@hotmail.com>
2025-12-09 19:44:30 -0500

    Update files

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error('No se encontró el elemento con id "root"');
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
